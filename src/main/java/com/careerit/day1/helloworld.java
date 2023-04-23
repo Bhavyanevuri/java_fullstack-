049c25c6aadedba6f50df99e3323ee035ab992a3
bhavyanevuri <nbreddy357@gmail.com>
2023-04-22 21:12:58 -0500

    Added extra line for same helloworld program.

--- src/main/java/com/careerit/day1/helloworld.java
+++ src/main/java/com/careerit/day1/helloworld.java
@@ -3,5 +3,8 @@ package com.careerit.day1;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("welcome to java world");
+        for(int i=1;i<=5;i++){
+            System.out.println("welcome user :"+i);
+        }
     }
 }
